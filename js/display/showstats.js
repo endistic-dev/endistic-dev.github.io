5512271f1bd80cf72672d1c9b1bdf213aba2737b
Endistic-DEV <endistic@gmail.com>
2022-11-12 12:32:20 -0500

    Improved Stats GUI

--- js/display/showstats.js
+++ js/display/showstats.js
@@ -1,5 +1,5 @@
 function getStatsText(stats, weapon) {
-  text =
+  /* text =
     "<br><b>Defensive Stats</b>"
     + "<br>Health: " + Math.round(stats["HEALTH"])
     + "<br> Defense: " + Math.round(stats["DEFENSE"])
@@ -36,6 +36,59 @@ function getStatsText(stats, weapon) {
     + "Wither Impact"
     + "<br>"
     + "Total Damage: " + Math.round(10000 * ((1 + (stats["INTELLIGENCE"] / 100) * 0.3) * 1 * 1)) + "<br>"
-    + "Total Shield: " + "Coming Soon"
+    + "Total Shield: " + "Coming Soon" */
+
+  let ability_damage = Math.round(((stats["WEAPON_ABILITY_DAMAGE"] * 1 + (stats["ABILITY_DAMAGE_PERCENT"] / 100)) * ((1 + (stats["INTELLIGENCE"] / 100)) * weapon["ability_damage_scaling"]) * 1 * 1));
+  let text = `
+    <div id="defensive-stats" class="stat-boxed-div">
+      <b>Defensive Stats</b>
+      <br>
+      <br>Health: ${Math.round(stats["HEALTH"])}
+      <br>Defense: ${Math.round(stats["DEFENSE"])}
+      <br>EHP: ${Math.round(stats["HEALTH"] * (1 + (stats["DEFENSE"] / 100)))}
+    </div>
+    <br>
+    <div id="melee-stats" class="stat-boxed-div">
+      <b>Melee Stats</b>
+      <br>
+      <br>Damage: ${Math.round(stats["DAMAGE"])}
+      <br>Strength: ${Math.round(stats["STRENGTH"])}
+      <br>Critical Damage: ${Math.round(stats["CRITICAL_DAMAGE"])}
+      <br>Critical Chance: ${Math.round(stats["CRITICAL_CHANCE"])}
+    </div>
+    <br>
+  `
+  if(stats["WEAPON_ABILITY_DAMAGE"] > 0) {
+     text = text + ` 
+      <div id="magic-stats" class="stat-boxed-div">
+       <b>Magic Stats</b>
+       <br>
+       <br>Base Damage: ${Math.round(stats["WEAPON_ABILITY_DAMAGE"])}
+       <br>Ability Scaling: ${Math.round(weapon["ability_damage_scaling"])}
+       <br>Ability Damage: ${Math.round(ability_damage)}
+     </div>
+     <br>`;
+  }
+  if(stats["MINING_SPEED"] > 0) {
+    text = text + `
+    <div id="mining-stats" class="stat-boxed-div">
+      <b>Mining Stats</b>
+      <br>
+      <br>Mining Speed: ${stats["MINING_SPEED"]}
+      <br>Mining Fortune: ${stats["MINING_FORTUNE"]}
+    </div>
+    <br>
+    `;
+  }
+  text = text + `
+    <div id="wither-impact" class="stat-boxed-div">
+      <b>Wither Impact</b>
+      <br>
+      <br>Damage: ${Math.round(10000 * ((1 + (stats["INTELLIGENCE"] / 100) * 0.3) * 1 * 1))}
+      <br>Shield: Coming soon.
+    </div>
+    <br>
+  `;
+  
   document.getElementById("stats").innerHTML = text
 }
